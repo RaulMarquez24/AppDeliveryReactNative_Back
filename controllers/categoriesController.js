9a6c3003f801a5b6eeaee0a45d0bfefc4af2509f
Raúl Márquez <raulmu785@gmail.com>
2023-07-12 15:48:43 +0200

    update category

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -50,6 +50,62 @@ module.exports = {
         });
     },
 
+    async updateWithImage(req, res) {
+        const category = JSON.parse(req.body.category); // CAPTURAR LOS DATOS QUE ENVIA EL CLIENTE
+
+        const files = req.files;
+
+        if (files.length > 0) {
+            const path = `image_${Date.now()}`;
+            const url = await storage(files[0], path);
+
+            if (url != undefined && url != null) {
+                category.image = url;
+            }
+        }
+
+        Category.update(category, (err, id) => {
+
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Error al editar la categoria',
+                    error: err
+                });
+            }
+
+            return res.status(201).json({
+                success: true,
+                message: `Categoria actualizada correctamente`,
+                data: `${id}`
+            });
+            
+        });
+    },
+
+    async update(req, res) {
+        
+        const category = req.body; // CAPTURAR LOS DATOS QUE ENVIA EL CLIENTE
+
+        Category.update(category, (err, id) => {
+
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Error al editar la categoria',
+                    error: err
+                });
+            }
+
+            return res.status(201).json({
+                success: true,
+                message: `Categoria actualizada correctamente`,
+                data: `${id}`
+            });
+            
+        });
+    },
+
     async delete(req, res) {
         const id = req.params.id;
         Category.delete(id, (err, data) =>{
